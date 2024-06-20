c532cf929ef5ca0d3ead9f256f9538d43e8669bf
psilberk <pablo.silberkasten@oracle.com>
2024-06-20 15:31:59 -0700

    Simple Sample GcpSecret

--- ojdbc-provider-gcp/src/main/java/oracle/jdbc/provider/gcp/configuration/GcpSecretProvider.java
+++ ojdbc-provider-gcp/src/main/java/oracle/jdbc/provider/gcp/configuration/GcpSecretProvider.java
@@ -1,7 +1,11 @@
 package oracle.jdbc.provider.gcp.configuration;
 
+import com.google.cloud.secretmanager.v1.Secret;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import oracle.jdbc.driver.OracleConfigurationJsonProvider;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 
@@ -11,8 +15,14 @@ import java.sql.SQLException;
  **/
 public class GcpSecretProvider extends OracleConfigurationJsonProvider {
   @Override
-  public InputStream getJson(String s) throws SQLException {
-    return null;
+  public InputStream getJson(String secretName) throws SQLException {
+    try {
+      SecretManagerServiceClient client = SecretManagerServiceClient.create();
+      Secret secret = client.getSecret(secretName);
+      return new ByteArrayInputStream(secret.toByteArray());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
