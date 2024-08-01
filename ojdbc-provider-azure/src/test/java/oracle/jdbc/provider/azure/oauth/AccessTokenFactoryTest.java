aff3f76955685afe0a36c222ac12491178c7a145
aallali <ayoub.allali@oracle.com>
2024-08-01 16:18:29 +0100

    change indentation to 2 space , and add documentation to test

--- ojdbc-provider-azure/src/test/java/oracle/jdbc/provider/azure/oauth/AccessTokenFactoryTest.java
+++ ojdbc-provider-azure/src/test/java/oracle/jdbc/provider/azure/oauth/AccessTokenFactoryTest.java
@@ -26,138 +26,154 @@ import static oracle.jdbc.provider.TestProperties.getOrAbort;
 import static oracle.jdbc.provider.azure.authentication.AzureAuthenticationMethod.SERVICE_PRINCIPLE;
 import static oracle.jdbc.provider.azure.authentication.TokenCredentialFactoryTest.buildParameterSet;
 import static org.junit.Assert.assertSame;
-
+/**
+ * Test class for AccessTokenFactory.
+ * This class contains unit tests to verify the functionality and thread safety of AccessToken caching.
+ */
 public class AccessTokenFactoryTest {
 
+  private CachedResourceFactory<Supplier<?extends AccessToken>> cachedResourceFactory;
+  private ParameterSet parameterSet;
 
+  /**
+   * Sets up the test environment by initializing the parameter set and cached resource factory.
+   */
+  @Before
+  public void setUp() {
+    parameterSet = getParameterSet("dummy"+0);
+    cachedResourceFactory = (CachedResourceFactory<Supplier<? extends AccessToken>>) AccessTokenFactory.getInstance();
+  }
+  /**
+   * Helper method to create a ParameterSet with a dummy value.
+   *
+   * @param dummyValue the dummy value to be added to the parameter set
+   * @return a  ParameterSet
+   */
+  private static ParameterSet getParameterSet(String dummyValue) {
+    Parameter<String> dummyParameter = Parameter.create();
 
-    private CachedResourceFactory<Supplier<?extends AccessToken>> cachedResourceFactory;
-    private ParameterSet parameterSet;
+    return buildParameterSet(SERVICE_PRINCIPLE)
+            .add(
+                    AzureTestProperty.AZURE_CLIENT_SECRET.name(),
+                    TokenCredentialFactory.CLIENT_SECRET,
+                    getOrAbort(AzureTestProperty.AZURE_CLIENT_SECRET))
+            .add("SCOPE", AccessTokenFactory.SCOPE, TestProperties.getOrAbort(AzureTestProperty.AZURE_TOKEN_SCOPE))
+            .add("dummy parameter",dummyParameter,dummyValue)
+            .build();
+  }
 
+  /**
+   * Tests the thread safety of the access token cache by running multiple threads requesting the token concurrently.
+   */
+  @Test
+  public void  testAccessTokenCacheThreadSafety() throws InterruptedException, ExecutionException {
+    ExecutorService executorService = Executors.newFixedThreadPool(16);
 
-    @Before
-    public void setUp() {
-        parameterSet = getParameterSet("dummy"+0);
-        cachedResourceFactory = (CachedResourceFactory<Supplier<? extends AccessToken>>) AccessTokenFactory.getInstance();
-    }
-
-    private static ParameterSet getParameterSet(String dummyValue) {
-        Parameter<String> dummyParameter = Parameter.create();
-
-        return buildParameterSet(SERVICE_PRINCIPLE)
-                .add(
-                        AzureTestProperty.AZURE_CLIENT_SECRET.name(),
-                        TokenCredentialFactory.CLIENT_SECRET,
-                        getOrAbort(AzureTestProperty.AZURE_CLIENT_SECRET))
-                .add("SCOPE", AccessTokenFactory.SCOPE, TestProperties.getOrAbort(AzureTestProperty.AZURE_TOKEN_SCOPE))
-                .add("dummy parameter",dummyParameter,dummyValue)
-                .build();
-    }
-
-
-    @Test
-    public void  testAccessTokenCacheThreadSafety() throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newFixedThreadPool(16);
-
-        CompletableFuture<AccessToken> [] futures = new CompletableFuture[16];
-        for (int i = 0; i < 16; i++) {
-            futures[i] = CompletableFuture.supplyAsync(()->{
-                AccessToken perviousToken = null;
-                for (int j = 0; j < 100; j++) {
-                    AccessToken currentAccessToken = cachedResourceFactory.request(parameterSet).getContent().get();
-                    if (perviousToken!= null && perviousToken != currentAccessToken){
-                        throw new AssertionError("AccessToken instance changed during iteration");
-                    }
-                    perviousToken = currentAccessToken;
-                }
-                return perviousToken;
-            }, executorService);
+    CompletableFuture<AccessToken> [] futures = new CompletableFuture[16];
+    for (int i = 0; i < 16; i++) {
+      futures[i] = CompletableFuture.supplyAsync(()->{
+        AccessToken perviousToken = null;
+        for (int j = 0; j < 100; j++) {
+          AccessToken currentAccessToken = cachedResourceFactory.request(parameterSet).getContent().get();
+          if (perviousToken!= null && perviousToken != currentAccessToken){
+            throw new AssertionError("AccessToken instance changed during iteration");
+          }
+          perviousToken = currentAccessToken;
         }
-
-        // wait all the futures to finish
-        CompletableFuture.allOf(futures).join();
-
-        AccessToken sharedToken = futures[0].get();
-        // test that all futures retrieved from different threads are same
-        for (CompletableFuture<AccessToken> future: futures){
-            assertSame("All threads should return same Access Token ",sharedToken,future.get());
-        }
-
-        executorService.shutdown();
+        return perviousToken;
+      }, executorService);
     }
 
+    // wait all the futures to finish
+    CompletableFuture.allOf(futures).join();
 
-
-    @Test
-    public void  testCachingMoreThanOneToken() throws InterruptedException, ExecutionException {
-        ParameterSet parameterSet2 = getParameterSet("dummy1");
-
-        AccessToken tokenX = cachedResourceFactory.request(parameterSet).getContent().get();
-        AccessToken tokenY = cachedResourceFactory.request(parameterSet2).getContent().get();
-
-        Assert.assertNotSame("This two Access Tokens should be different  ",tokenX,tokenY);
+    AccessToken sharedToken = futures[0].get();
+    // test that all futures retrieved from different threads are same
+    for (CompletableFuture<AccessToken> future: futures){
+      assertSame("All threads should return same Access Token ",sharedToken,future.get());
     }
 
-
-    @Test
-    public void testTokenUniquenessWithMultipleScopesAndThreads() throws InterruptedException, ExecutionException {
-       // create 4 parameter set each one  will have a 16 thread that have 100 iteration to request token
-        int numParameterSet = 4;
-        int numThreads = 16;
-        int numIterations = 100;
+    executorService.shutdown();
+  }
 
 
-        ExecutorService executorService = Executors.newFixedThreadPool(numThreads*numParameterSet);
+  /**
+   * Tests caching of more than one token by using different parameter sets.
+   */
+  @Test
+  public void  testCachingMoreThanOneToken() throws InterruptedException, ExecutionException {
+    ParameterSet parameterSet2 = getParameterSet("dummy1");
 
-        Map<String,CompletableFuture<AccessToken>[]>  completableFutureMap = new ConcurrentHashMap<>();
-        for (int i=0;i<numParameterSet;i++){
-            String dummyParameter = "dummy"+i;
-            ParameterSet parameterSet = getParameterSet(dummyParameter);
-            CompletableFuture<AccessToken>[] futures =new CompletableFuture[numThreads];
+    AccessToken tokenX = cachedResourceFactory.request(parameterSet).getContent().get();
+    AccessToken tokenY = cachedResourceFactory.request(parameterSet2).getContent().get();
 
-            for (int j=0;j<numThreads;j++){
-                futures[j] = CompletableFuture.supplyAsync(()->{
-                    AccessToken previousAccessToken = null;
-                    for (int k = 0; k < numIterations; k++) {
-                        AccessToken currentAccessToken = cachedResourceFactory.request(parameterSet).getContent().get();
-                        if (previousAccessToken != null && previousAccessToken != currentAccessToken){
-                            throw new AssertionError("This Access Token instances should be same  ");
-                        }
-                        previousAccessToken = currentAccessToken;
-                    }
-                    return previousAccessToken;
+    Assert.assertNotSame("This two Access Tokens should be different  ",tokenX,tokenY);
+  }
 
-                },executorService);
+  /**
+   * Tests the uniqueness of tokens with multiple scopes and threads by running multiple threads
+   * with different parameter sets and verifying that tokens are unique per parameter set.
+   */
+  @Test
+  public void testTokenUniquenessWithMultipleScopesAndThreads() throws InterruptedException, ExecutionException {
+    // create 4 parameter set each one  will have a 16 thread that have 100 iteration to request token
+    int numParameterSet = 4;
+    int numThreads = 16;
+    int numIterations = 100;
+
+
+    ExecutorService executorService = Executors.newFixedThreadPool(numThreads*numParameterSet);
+
+    Map<String,CompletableFuture<AccessToken>[]>  completableFutureMap = new ConcurrentHashMap<>();
+    for (int i=0;i<numParameterSet;i++){
+      String dummyParameter = "dummy"+i;
+      ParameterSet parameterSet = getParameterSet(dummyParameter);
+      CompletableFuture<AccessToken>[] futures =new CompletableFuture[numThreads];
+
+      for (int j=0;j<numThreads;j++){
+        futures[j] = CompletableFuture.supplyAsync(()->{
+          AccessToken previousAccessToken = null;
+          for (int k = 0; k < numIterations; k++) {
+            AccessToken currentAccessToken = cachedResourceFactory.request(parameterSet).getContent().get();
+            if (previousAccessToken != null && previousAccessToken != currentAccessToken){
+              throw new AssertionError("This Access Token instances should be same  ");
             }
-            completableFutureMap.putIfAbsent(dummyParameter,futures);
+            previousAccessToken = currentAccessToken;
+          }
+          return previousAccessToken;
 
-        }
-
-        // wait all futures to join
-        for (CompletableFuture<AccessToken>[] futures : completableFutureMap.values()) {
-            CompletableFuture.allOf(futures).join();
-        }
-
-        // test if all instances that have same parameter set  are same .
-        for (int i = 0; i < numParameterSet; i++) {
-            String dummyParameter = "dummy"+i;
-            CompletableFuture<AccessToken>[] parameterSetFutures = completableFutureMap.get(dummyParameter);
-            AccessToken firstAccessToken = parameterSetFutures[0].get();
-            for (Future<AccessToken> currentAccessToken : parameterSetFutures){
-                Assert.assertSame("This instances should be same ,because we use same parameter set  ",firstAccessToken,currentAccessToken.get());
-            }
-        }
-
-        // test that all instances that doesn't have same parameter set will be different
-        Set<AccessToken> tokenSet = new HashSet<>();
-        for (CompletableFuture<AccessToken>[] future : completableFutureMap.values()){
-            AccessToken accessToken = future[0].get();
-            if (!tokenSet.add(accessToken)){
-                throw new AssertionError("This tokens should be unique per parameter set ");
-            }
-        }
+        },executorService);
+      }
+      completableFutureMap.putIfAbsent(dummyParameter,futures);
 
     }
+
+    // wait all futures to join
+    for (CompletableFuture<AccessToken>[] futures : completableFutureMap.values()) {
+      CompletableFuture.allOf(futures).join();
+    }
+
+    // test if all instances that have same parameter set  are same .
+    for (int i = 0; i < numParameterSet; i++) {
+      String dummyParameter = "dummy"+i;
+      CompletableFuture<AccessToken>[] parameterSetFutures = completableFutureMap.get(dummyParameter);
+      AccessToken firstAccessToken = parameterSetFutures[0].get();
+      for (Future<AccessToken> currentAccessToken : parameterSetFutures){
+        Assert.assertSame("This instances should be same ,because we use same parameter set  ",firstAccessToken,
+                currentAccessToken.get());
+      }
+    }
+
+    // test that all instances that doesn't have same parameter set will be different
+    Set<AccessToken> tokenSet = new HashSet<>();
+    for (CompletableFuture<AccessToken>[] future : completableFutureMap.values()){
+      AccessToken accessToken = future[0].get();
+      if (!tokenSet.add(accessToken)){
+        throw new AssertionError("This tokens should be unique per parameter set ");
+      }
+    }
+
+  }
 
 }
 
--- ojdbc-provider-oci/src/test/java/oracle/jdbc/provider/oci/oaut/AccessTokenFactoryTest.java
+++ ojdbc-provider-oci/src/test/java/oracle/jdbc/provider/oci/oaut/AccessTokenFactoryTest.java
@@ -13,7 +13,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -29,147 +28,157 @@ import static oracle.jdbc.provider.TestProperties.getOrAbort;
 import static oracle.jdbc.provider.oci.AuthenticationDetailsFactoryTest.buildParameterSet;
 import static org.junit.Assert.assertSame;
 
-
+/**
+ * Test class for AccessTokenFactory
+ * This class has some unit test to verify the functionality and thread safety
+ * of accessToken caching.
+ */
 public class AccessTokenFactoryTest {
 
-    private CachedResourceFactory<Supplier<?extends AccessToken>> cachedResourceFactory;
-    private ParameterSet parameterSetMock;
+  private CachedResourceFactory<Supplier<?extends AccessToken>> cachedResourceFactory;
+  private ParameterSet parameterSetMock;
 
+  /**
+   * Sets up the test environment by initializing the parameter set and cached resource factory.
+   */
+  @Before
+  public void setUp() {
+    parameterSetMock = getParameterSet("dummy0");
+    cachedResourceFactory = (CachedResourceFactory<Supplier<? extends AccessToken>>)AccessTokenFactory.getInstance();
+  }
+  /**
+   * Helper method to create a ParameterSet with a dummy value.
+   *
+   * @param dummyValue the dummy value to be added to the parameter set
+   * @return a  ParameterSet
+   */
+  private static ParameterSet getParameterSet(String dummyValue) {
+    Parameter<String> dummyParameter = Parameter.create();
+    return buildParameterSet(AuthenticationMethod.CONFIG_FILE)
+            .add(
+                    OciTestProperty.OCI_CONFIG_FILE.name(),
+                    AuthenticationDetailsFactory.CONFIG_FILE_PATH,
+                    getOrAbort(OciTestProperty.OCI_CONFIG_FILE))
+            .add(
+                    OciTestProperty.OCI_CONFIG_PROFILE.name(),
+                    AuthenticationDetailsFactory.CONFIG_PROFILE,
+                    getOrAbort(OciTestProperty.OCI_CONFIG_PROFILE))
+            .add("SCOPE", AccessTokenFactory.SCOPE, TestProperties.getOrAbort(OciTestProperty.OCI_TOKEN_SCOPE))
+            .add("dummy parameter",dummyParameter,dummyValue)
+            .build();
+  }
 
-    @Before
-    public void setUp() throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-        parameterSetMock = getParameterSet("dummy0");
-        cachedResourceFactory = (CachedResourceFactory<Supplier<? extends AccessToken>>)AccessTokenFactory.getInstance();
-    }
+  /**
+   * Tests the thread safety of the access token cache by running multiple threads requesting the same token concurrently.
+   */
+  @Test
+  public void  testAccessTokenCacheThreadSafety() throws InterruptedException, ExecutionException {
+    // Act
+    ExecutorService executorService = Executors.newFixedThreadPool(16);
 
-    private static ParameterSet getParameterSet(String dummyValue) {
-        Parameter<String> dummyParameter = Parameter.create();
-        return buildParameterSet(AuthenticationMethod.CONFIG_FILE)
-                .add(
-                        OciTestProperty.OCI_CONFIG_FILE.name(),
-                        AuthenticationDetailsFactory.CONFIG_FILE_PATH,
-                        getOrAbort(OciTestProperty.OCI_CONFIG_FILE))
-                .add(
-                        OciTestProperty.OCI_CONFIG_PROFILE.name(),
-                        AuthenticationDetailsFactory.CONFIG_PROFILE,
-                        getOrAbort(OciTestProperty.OCI_CONFIG_PROFILE))
-                .add("SCOPE", AccessTokenFactory.SCOPE, TestProperties.getOrAbort(OciTestProperty.OCI_TOKEN_SCOPE))
-                .add("dummy parameter",dummyParameter,dummyValue)
-                .build();
-    }
-
-
-    @Test
-    public void  testAccessTokenCacheThreadSafety() throws InterruptedException, ExecutionException {
-        // Act
-        ExecutorService executorService = Executors.newFixedThreadPool(16);
-
-        CompletableFuture<AccessToken>[] futures = new CompletableFuture[16];
-        for (int i = 0; i < 16; i++) {
-            futures[i] = CompletableFuture.supplyAsync(()->{
-                AccessToken perviousToken = null;
-                for (int j = 0; j < 100; j++) {
-                    AccessToken currentAccessToken = cachedResourceFactory.request(parameterSetMock).getContent().get();
-                    if (perviousToken!= null && perviousToken != currentAccessToken){
-                        throw new AssertionError("AccessToken instance changed during iteration");
-                    }
-                    perviousToken = currentAccessToken;
-                }
-                return perviousToken;
-            }, executorService);
+    CompletableFuture<AccessToken>[] futures = new CompletableFuture[16];
+    for (int i = 0; i < 16; i++) {
+      futures[i] = CompletableFuture.supplyAsync(()->{
+        AccessToken perviousToken = null;
+        for (int j = 0; j < 100; j++) {
+          AccessToken currentAccessToken = cachedResourceFactory.request(parameterSetMock).getContent().get();
+          if (perviousToken!= null && perviousToken != currentAccessToken){
+            throw new AssertionError("AccessToken instance changed during iteration");
+          }
+          perviousToken = currentAccessToken;
         }
-
-        // wait all the futures to finish
-        CompletableFuture.allOf(futures).join();
-
-        AccessToken sharedToken = futures[0].get();
-        // test that all futures retrieved from different threads are same
-        for (CompletableFuture<AccessToken> future: futures){
-            assertSame("All threads should return same Access Token ",sharedToken,future.get());
-        }
-
-        executorService.shutdown();
+        return perviousToken;
+      }, executorService);
     }
 
+    // wait all the futures to finish
+    CompletableFuture.allOf(futures).join();
 
-
-    @Test
-    public void  testCachingMoreThanOneToken() throws InterruptedException, ExecutionException {
-
-        ParameterSet parameterSet2 = getParameterSet("dummy1");
-        AccessToken tokenX = cachedResourceFactory.request(parameterSetMock).getContent().get();
-        AccessToken tokenY = cachedResourceFactory.request(parameterSet2).getContent().get();
-
-        Assert.assertNotSame("This two Access Tokens should be different  ",tokenX,tokenY);
+    AccessToken sharedToken = futures[0].get();
+    // test that all futures retrieved from different threads are same
+    for (CompletableFuture<AccessToken> future: futures){
+      assertSame("All threads should return same Access Token ",sharedToken,future.get());
     }
 
+    executorService.shutdown();
+  }
 
-    @Test
-    public void testTokenUniquenessWithMultipleScopesAndThreads() throws InterruptedException, ExecutionException {
-        // create 4 parameter set each one  will have a 16 thread that have 100 iteration to request token
-        int numParameterSet = 4;
-        int numThreads = 16;
-        int numIterations = 100;
 
-        ExecutorService executorService = Executors.newFixedThreadPool(numThreads*numParameterSet);
+  /**
+   * Tests caching of more than one token by using different parameter sets.
+   */
+  @Test
+  public void  testCachingMoreThanOneToken() throws InterruptedException, ExecutionException {
 
-        Map<String,CompletableFuture<AccessToken>[]> completableFutureMap = new ConcurrentHashMap<>();
-        for (int i=0;i<numParameterSet;i++){
-            String dummyParameter = "dummy"+i;
-            ParameterSet parameterSet = getParameterSet(dummyParameter);
-            CompletableFuture<AccessToken>[] futures =new CompletableFuture[numThreads];
+    ParameterSet parameterSet2 = getParameterSet("dummy1");
+    AccessToken tokenX = cachedResourceFactory.request(parameterSetMock).getContent().get();
+    AccessToken tokenY = cachedResourceFactory.request(parameterSet2).getContent().get();
 
-            for (int j=0;j<numThreads;j++){
-                futures[j] = CompletableFuture.supplyAsync(()->{
-                    AccessToken previousAccessToken = null;
-                    for (int k = 0; k < numIterations; k++) {
-                        AccessToken currentAccessToken = cachedResourceFactory.request(parameterSet).getContent().get();
-                        if (previousAccessToken != null && previousAccessToken != currentAccessToken){
-                            throw new AssertionError("This Access Token instances should be same  ");
-                        }
-                        previousAccessToken = currentAccessToken;
-                    }
-                    return previousAccessToken;
+    Assert.assertNotSame("This two Access Tokens should be different  ",tokenX,tokenY);
+  }
 
-                },executorService);
+  /**
+   * Tests the uniqueness of tokens with multiple scopes and threads by running multiple threads
+   * with different parameter sets and verifying that tokens are unique per parameter set.
+   */
+  @Test
+  public void testTokenUniquenessWithMultipleScopesAndThreads() throws InterruptedException, ExecutionException {
+    // create 4 parameter set each one  will have a 16 thread that have 100 iteration to request token
+    int numParameterSet = 4;
+    int numThreads = 16;
+    int numIterations = 100;
+
+    ExecutorService executorService = Executors.newFixedThreadPool(numThreads*numParameterSet);
+
+    Map<String,CompletableFuture<AccessToken>[]> completableFutureMap = new ConcurrentHashMap<>();
+    for (int i=0;i<numParameterSet;i++){
+      String dummyParameter = "dummy"+i;
+      ParameterSet parameterSet = getParameterSet(dummyParameter);
+      CompletableFuture<AccessToken>[] futures =new CompletableFuture[numThreads];
+
+      for (int j=0;j<numThreads;j++){
+        futures[j] = CompletableFuture.supplyAsync(()->{
+          AccessToken previousAccessToken = null;
+          for (int k = 0; k < numIterations; k++) {
+            AccessToken currentAccessToken = cachedResourceFactory.request(parameterSet).getContent().get();
+            if (previousAccessToken != null && previousAccessToken != currentAccessToken){
+              throw new AssertionError("This Access Token instances should be same  ");
             }
-            completableFutureMap.putIfAbsent(dummyParameter,futures);
+            previousAccessToken = currentAccessToken;
+          }
+          return previousAccessToken;
 
-        }
-
-        // wait all futures to join
-        for (CompletableFuture<AccessToken>[] futures : completableFutureMap.values()) {
-            CompletableFuture.allOf(futures).join();
-        }
-
-        // test if all instances that have same parameter set  are same .
-        for (int i = 0; i < numParameterSet; i++) {
-            String dummyParameter = "dummy"+i;
-            CompletableFuture<AccessToken>[] parameterSetFutures = completableFutureMap.get(dummyParameter);
-            AccessToken firstAccessToken = parameterSetFutures[0].get();
-            for (Future<AccessToken> currentAccessToken : parameterSetFutures){
-                Assert.assertSame("This instances should be same ,because we use same parameter set  ",firstAccessToken,currentAccessToken.get());
-            }
-        }
-
-        // test that all instances that doesn't have same parameter set will be different
-        Set<AccessToken> tokenSet = new HashSet<>();
-        for (CompletableFuture<AccessToken>[] future : completableFutureMap.values()){
-            AccessToken accessToken = future[0].get();
-            if (!tokenSet.add(accessToken)){
-                throw new AssertionError("This tokens should be unique per parameter set ");
-            }
-        }
+        },executorService);
+      }
+      completableFutureMap.putIfAbsent(dummyParameter,futures);
 
     }
 
+    // wait all futures to join
+    for (CompletableFuture<AccessToken>[] futures : completableFutureMap.values()) {
+      CompletableFuture.allOf(futures).join();
+    }
 
+    // test if all instances that have same parameter set  are same .
+    for (int i = 0; i < numParameterSet; i++) {
+      String dummyParameter = "dummy"+i;
+      CompletableFuture<AccessToken>[] parameterSetFutures = completableFutureMap.get(dummyParameter);
+      AccessToken firstAccessToken = parameterSetFutures[0].get();
+      for (Future<AccessToken> currentAccessToken : parameterSetFutures){
+        Assert.assertSame("This instances should be same ,because we use same parameter set  ",firstAccessToken,currentAccessToken.get());
+      }
+    }
 
+    // test that all instances that doesn't have same parameter set will be different
+    Set<AccessToken> tokenSet = new HashSet<>();
+    for (CompletableFuture<AccessToken>[] future : completableFutureMap.values()){
+      AccessToken accessToken = future[0].get();
+      if (!tokenSet.add(accessToken)){
+        throw new AssertionError("This tokens should be unique per parameter set ");
+      }
+    }
 
-
-
-
-
+  }
 }
 
 
