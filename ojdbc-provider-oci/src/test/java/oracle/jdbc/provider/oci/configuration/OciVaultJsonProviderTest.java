13071d3533d3af43e2eaa5e0a2173963e433a333
Kaiyuan Li <KAIYUAN.LI@ORACLE.COM>
2024-09-11 11:31:05 -0700

    add test with key option in OciVaultJsonProviderTest

--- ojdbc-provider-oci/src/test/java/oracle/jdbc/provider/oci/configuration/OciVaultJsonProviderTest.java
+++ ojdbc-provider-oci/src/test/java/oracle/jdbc/provider/oci/configuration/OciVaultJsonProviderTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Verifies the {@link OciVaultJsonProvider} as implementing behavior
  * specified by its JavaDoc.
- */
+ **/
 
 public class OciVaultJsonProviderTest {
 
@@ -28,23 +28,38 @@ public class OciVaultJsonProviderTest {
    * Verifies the AUTHENTICATION=OCI_DEFAULT parameter setting.
    * This test will fail if (~/.oci/config) or (~/.oraclebmc/config) or the
    * environmental variable OCI_CONFIG_FILE is not set.
-   */
+   **/
   @Test
   public void testDefaultAuthentication() throws SQLException {
-    verifyProperties("AUTHENTICATION=OCI_DEFAULT");
+    String[] options = new String[] {"AUTHENTICATION=OCI_DEFAULT"};
+
+    verifyProperties(composeUrl(
+      TestProperties.getOrAbort(OciTestProperty.OCI_PASSWORD_PAYLOAD_OCID),
+      options));
+  }
+
+  /**
+   * Verifies the AUTHENTICATION=OCI_DEFAULT parameter setting with key option.
+   **/
+  @Test
+  public void testDefaultAuthenticationWithKeyOption() throws SQLException {
+    String[] options = new String[] {
+      "AUTHENTICATION=OCI_DEFAULT",
+      "key=" + TestProperties.getOrAbort(OciTestProperty.OCI_PASSWORD_PAYLOAD_OCID_KEY)};
+
+    verifyProperties(composeUrl(
+      TestProperties.getOrAbort(OciTestProperty.OCI_PASSWORD_PAYLOAD_OCID_MULTIPLE_KEYS),
+      options));
   }
 
   /** verifies a properties object returned with a URL with the given options **/
-  private static void verifyProperties(String... options) throws SQLException {
-    Properties properties = PROVIDER
-      .getConnectionProperties(composeUrl(options));
+  private static void verifyProperties(String url) throws SQLException {
+    Properties properties = PROVIDER.getConnectionProperties(url);
 
     assertNotNull(properties);
   }
 
-  private static String composeUrl(String... options) {
-    return String.format("%s?%s",
-      TestProperties.getOrAbort(OciTestProperty.OCI_PASSWORD_PAYLOAD_OCID),
-      String.join("&", options));
+  private static String composeUrl(String baseUrl, String... options) {
+    return String.format("%s?%s", baseUrl, String.join("&", options));
   }
 }
