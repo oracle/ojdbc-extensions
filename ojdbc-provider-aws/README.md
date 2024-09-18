9222213e6059327365a6db00260dc9631c5da177
pablo_silberkasten <pablo.silberkasten@oracle.com>
2024-09-18 16:40:22 -0700

    First aws provider

---
+++ ojdbc-provider-aws/README.md
@@ -0,0 +1,7 @@
+# Oracle JDBC Providers for AWS
+
+This module contains providers for integration between Oracle JDBC and AWS.
+
+## Centralized Config Providers
+
+Finish me
---
+++ ojdbc-provider-aws/src/main/java/oracle/jdbc/provider/aws/configuration/AWSS3ConfigurationProvider.java
@@ -0,0 +1,43 @@
+package oracle.jdbc.provider.aws.configuration;
+
+import oracle.jdbc.driver.OracleConfigurationJsonProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+public class AWSS3ConfigurationProvider extends OracleConfigurationJsonProvider {
+
+    @Override
+    public InputStream getJson(String s3Url) throws SQLException {
+
+        URI uri = null;
+        try {
+            uri = new URI(s3Url);
+        } catch (URISyntaxException uriSyntaxException) {
+            throw new SQLException(uriSyntaxException);
+        }
+
+        try (S3Client client = S3Client.builder().build()) {
+
+            String bucketName = uri.getHost();
+            String objectKey = uri.getPath()
+                    .substring(1);
+
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectKey)
+                    .build();
+
+            return client.getObjectAsBytes(getObjectRequest).asInputStream();
+        }
+    }
+
+    @Override
+    public String getType() {
+        return "awss3";
+    }
+}
