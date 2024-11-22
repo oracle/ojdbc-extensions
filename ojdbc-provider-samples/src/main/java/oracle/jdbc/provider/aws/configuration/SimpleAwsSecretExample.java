d962c74d44b692c2a8c12d20c5993d2bf3d92291
ting-lan-wang <ting-lan.wang@oracle.com>
2024-11-22 16:54:28 -0500

    Rename AwsS3ConfigurationProvider and remove AwsAppConfigProvider

--- ojdbc-provider-samples/src/main/java/oracle/jdbc/provider/aws/configuration/SimpleAwsSecretExample.java
+++ ojdbc-provider-samples/src/main/java/oracle/jdbc/provider/aws/configuration/SimpleAwsSecretExample.java
@@ -13,7 +13,7 @@ public class SimpleAwsSecretExample {
 
     // Sample default URL if non present
     if (args.length == 0) {
-      url = "jdbc:oracle:thin:@config-awssecret://payload_ojdbc_base64_adb";
+      url = "jdbc:oracle:thin:@config-awssecret://{secret-name}";
     } else {
       url = args[0];
     }
