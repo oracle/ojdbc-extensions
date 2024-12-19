268481d312616a542f341532368fbe936c4b967f
Bidyadhar Mohanty <bidyadhar.mohanty@oracle.com>
2024-12-19 16:24:39 +0530

    add "java.net" package from the skip list.

--- ojdbc-provider-jackson-oson/src/main/java/oracle/jdbc/provider/oson/Util.java
+++ ojdbc-provider-jackson-oson/src/main/java/oracle/jdbc/provider/oson/Util.java
@@ -16,6 +16,7 @@ public class Util {
         || packageName.startsWith("java.time")
         || packageName.startsWith("java.math")
         || packageName.startsWith("java.security")
+        || packageName.startsWith("java.net")
         || clazz.isArray()) {
             return true;
         }
