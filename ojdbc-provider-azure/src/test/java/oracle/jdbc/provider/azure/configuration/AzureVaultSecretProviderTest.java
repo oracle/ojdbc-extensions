69d538cd9c97b5c7f34510a77785b071eca68684
Fernanda Meheust <fernanda.meheust@oracle.com>
2025-02-06 22:08:00 +0100

    Fixed test and provider

--- ojdbc-provider-azure/src/test/java/oracle/jdbc/provider/azure/configuration/AzureVaultSecretProviderTest.java
+++ ojdbc-provider-azure/src/test/java/oracle/jdbc/provider/azure/configuration/AzureVaultSecretProviderTest.java
@@ -115,7 +115,6 @@ public class AzureVaultSecretProviderTest {
     secretProperties.put("AZURE_CLIENT_ID", clientId);
     secretProperties.put("AZURE_CLIENT_SECRET", clientSecret);
     secretProperties.put("AZURE_TENANT_ID", tenantId);
-    secretProperties.put("type", "azurevault");
     secretProperties.put("value", constructSecretUri(vaultUrl, secretName));
     return secretProperties;
   }
